218adbd5e1d084463fdacd32403c317f3d99afb4
samuelblo <samuelblo@MacBook-Pro-di-Samuel-Blo.local>
2014-07-15 10:21:35 +0200

    Add_File in blocchi

--- TestJob-Dirac.py
+++ TestJob-Dirac.py
@@ -9,6 +9,7 @@ import uuid
 
 
 
+
 class stepOne(threading.Thread):
     # Costruttore
     def __init__(self, id, str_uuid):
@@ -23,6 +24,7 @@ class stepOne(threading.Thread):
         # -------------------------------
         self.time = []
         self.transName = "transName" + str(self.id) + str(self.uuid_str)
+        
         self.time_start = time.time()
         self.dl_count = 0
         self.deadlock = 1
@@ -40,26 +42,61 @@ class stepOne(threading.Thread):
                     print "((( THREAD ",self.id,"))) \033[1;31m#### ERROR: Trasformazione non creata ####\033[1;m"
                     print self.res['Message']
         self.time.append( time.time() -  self.time_start )
+        
         self.transID = self.res['Value']
         # Si mette il valore del TransID in coda alla lista transID del main 
         transID[self.id-1] = self.transID
     
+
         # -------------------------------------------------------------------
         # Si aggiungono [random(MIN_FILE,MAX_FILE)] File alla Trasformazione
         # ------------------------------------------------------------------- 
         self.n_file = random.randint(MIN_FILE,MAX_FILE)
         print "((( THREAD ",self.id,"))) Sto creando ",self.n_file," File .... "
-        self.lfns=[]
-        # Si assembla il nome del file per generarli con nomi diversi
+        self.lfns = []
+        # Si genera la lista con i nomi dei File da inserire
         for n in range( self.n_file ):
             #self.lfns.append( "/aa/lfn." + str(n) + "." + str(self.id) + ".txt" )
-            self.lfns.append( "/aa/lfn." + str(n) + str(self.id) + str(self.uuid_str) + ".txt" )
+            self.lfns.append( "/aa/lfn." + str(n) + "." + str(self.id) + "." + str(self.uuid_str) + ".txt" )
+
         self.time_start = time.time()
+        self.index_min = 0
+        self.index_max = BLOCK_ADD_FILE
+        # Si controlla se BLOCK_ADD_FILE e' minore della grandella della lista dei nomi dei File
+        if (self.index_max < (len(self.lfns)-1)):
+            # si entra nel ciclo while
+            self.exit = 0
+        else:
+            # si salta il ciclo while
+            self.exit = 1
+        # Si divide la lista con i nomi dei File in blocchi da BLOCK_ADD_FILE
+        while(self.exit == 0):
+            self.deadlock = 1
+            while (self.deadlock == 1):
+                self.lfns_temp = []
+                self.lfns_temp = self.lfns[self.index_min:self.index_max]
+                self.res = transClient.addFilesToTransformation( self.transID, self.lfns_temp )
+                if self.res['OK']==True:
+                    self.deadlock = 0
+                else:
+                    if (self.res["Message"] == "Execution failed.: ( 1213: Deadlock found when trying to get lock; try restarting transaction )"):
+                        self.deadlock = 1
+                        self.dl_count += 1
+                    else:
+                        self.deadlock = 0
+                        print "((( THREAD ",self.id,"))) \033[1;31m#### ERROR: File non creati ####\033[1;m"
+                        print self.res['Message']
+            self.index_min = self.index_max
+            if ((self.index_max + BLOCK_ADD_FILE)<(len(self.lfns)-1)):
+                self.index_max += BLOCK_ADD_FILE
+            else:
+                self.exit = 1
         self.deadlock = 1
         while (self.deadlock == 1):
-            self.res = transClient.addFilesToTransformation( self.transID, self.lfns )
+            self.lfns_temp = []
+            self.lfns_temp = self.lfns[self.index_min:(len(self.lfns))]
+            self.res = transClient.addFilesToTransformation( self.transID, self.lfns_temp )
             if self.res['OK']==True:
-                print "((( THREAD ",self.id,"))) **** Creati ",self.n_file," File  ****"
                 self.deadlock = 0
             else:
                 if (self.res["Message"] == "Execution failed.: ( 1213: Deadlock found when trying to get lock; try restarting transaction )"):
@@ -70,8 +107,11 @@ class stepOne(threading.Thread):
                     print "((( THREAD ",self.id,"))) \033[1;31m#### ERROR: File non creati ####\033[1;m"
                     print self.res['Message']
         self.time.append(time.time() -  self.time_start)
+
+        print "((( THREAD ",self.id,"))) **** Creati ",self.n_file," File  ****"
         # Si mette la lista dei nomi dei file in coda alla lista lfns del main
         lfns[self.id-1] = self.lfns
+
 
         # --------------------------------------------------------------------------
         # Si aggiunge un Task ogni [random(MIN_FILE_TO_TASK,MAX_FILE_TO_TASK)] File
@@ -81,6 +121,7 @@ class stepOne(threading.Thread):
         self.max = random.randint(MIN_FILE_TO_TASK,MAX_FILE_TO_TASK)
         self.taskID = []
         self.n_task = 0
+
         self.time_start = time.time()
         while( self.max <= self.n_file ):
             self.lfns_tmp = self.lfns[self.min:self.max]
@@ -103,6 +144,7 @@ class stepOne(threading.Thread):
             self.min = self.max
             self.max = self.max + self.rand
         self.time.append(time.time() -  self.time_start)
+
         print "((( THREAD ",self.id,"))) **** Creati ",self.n_task," Tasks ****"
         # Si mettono in coda alle rispettive liste del main i valori di taskID, n_task (numero tot di task creati), min e n_file (numero tot di file)
         taskID[self.id-1] = self.taskID
@@ -111,6 +153,7 @@ class stepOne(threading.Thread):
         n_file[self.id-1] = self.n_file
         times_s1[self.id-1] = self.time
         dl_count_step1[self.id-1] = self.dl_count
+
 
 
 
@@ -136,6 +179,7 @@ class stepTwo(threading.Thread):
         self.taskID_tmp = self.taskID[0:self.rand]
         self.dl_count = 0
         self.deadlock = 1
+
         self.time_start = time.time()
         while (self.deadlock == 1):
             self.res = transClient.setTaskStatus(self.transID, self.taskID_tmp, self.status)
@@ -152,12 +196,14 @@ class stepTwo(threading.Thread):
                     print self.res['Message']
         self.time.append(time.time() -  self.time_start)
 
+
         # -------------------------------------------------------------------------------------------------------------
         # Si setta lo stato dei primi [random(MIN_CHANGE_STATUS_FILE,MAX_CHANGE_STATUS_FILE)] File a 'Status_Modified'
         # -------------------------------------------------------------------------------------------------------------
         self.rand = random.randint(MIN_CHANGE_STATUS_FILE,MAX_CHANGE_STATUS_FILE)
         self.newLFNsStatus = "Status_Modified_TH"+str(self.id)
         self.lfns_tmp = self.lfns[0:self.rand]
+
         self.time_start = time.time()
         self.deadlock = 1
         while (self.deadlock == 1):
@@ -174,8 +220,10 @@ class stepTwo(threading.Thread):
                     print "((( THREAD ",self.id,"))) \033[1;31m#### ERROR: Stato dei File non modificati ####\033[1;m"
                     print self.res['Message']
         self.time.append(time.time() -  self.time_start)
+
         times_s2[self.id-1] = self.time
         dl_count_step2[self.id-1] = self.dl_count
+
 
 
     
@@ -196,6 +244,7 @@ class stepThree(threading.Thread):
         # get stato dei Task di una Trasformazione
         # -----------------------------------------
         self.time = []
+
         self.time_start = time.time()
         self.dl_count = 0
         self.deadlock = 1
@@ -233,6 +282,7 @@ class stepThree(threading.Thread):
                     print self.res['Message']
         self.time.append(time.time() -  self.time_start)
       
+
         # ---------------------------------
         # Modifica dello stato di un File
         #
@@ -240,6 +290,7 @@ class stepThree(threading.Thread):
         # ---------------------------------
         self.newLFNsStatus = "Second_Modified"
         self.lfns_tmp = self.lfns[0:1]
+
         self.time_start = time.time()
         self.deadlock = 1
         while (self.deadlock == 1):
@@ -273,8 +324,10 @@ class stepThree(threading.Thread):
                     print "((( THREAD ",self.id,"))) \033[1;31m#### ERROR: Task non aggiunto ####\033[1;m"
                     print self.res['Message']
         self.time.append(time.time() -  self.time_start)
+        
         times_s3[self.id-1] = self.time
         dl_count_step3[self.id-1] = self.dl_count
+
 
 
 
@@ -294,6 +347,7 @@ class stepFour(threading.Thread):
         self.time = []
         self.dl_count = 0
         self.c = 0
+        
         self.time_start = time.time()
         for i in range (READ_TRANS_STATUS):
             self.deadlock = 1
@@ -312,9 +366,11 @@ class stepFour(threading.Thread):
                         print "((( THREAD ",self.id,"))) \033[1;31m#### ERROR: Stato della Trasformazione impossibile da recuperare ####\033[1;m"
                         print self.res['Message']
         self.time.append(time.time() -  self.time_start)
+        
         print "((( THREAD ",self.id,"))) **** Stato della Trasformazione recuperato (x",self.c,") ****"
         
         self.c = 0
+        
         self.time_start = time.time()
         for i in range (READ_TASK_STATUS):
             self.deadlock = 1
@@ -333,10 +389,11 @@ class stepFour(threading.Thread):
                         print "((( THREAD ",self.id,"))) \033[1;31m#### ERROR: Stato del Task impossibile da recuperare ####\033[1;m"
                         print self.res['Message']
         self.time.append(time.time() -  self.time_start)
+        
         print "((( THREAD ",self.id,"))) **** Stato dei Task recuperati (x",self.c,") ****"
-
         times_s4[self.id-1] = self.time
         dl_count_step4[self.id-1] = self.dl_count
+
 
 
 
@@ -378,23 +435,24 @@ if __name__ == "__main__":
     # ----------------------------
     MIN_FILE = 100
     MAX_FILE = 100
+    BLOCK_ADD_FILE = 10000
 
     MIN_FILE_TO_TASK = 10
     MAX_FILE_TO_TASK = 10
 
-    MIN_CHANGE_STATUS_TT = 5
-    MAX_CHANGE_STATUS_TT = 5
+    MIN_CHANGE_STATUS_TT = 2
+    MAX_CHANGE_STATUS_TT = 2
 
-    MIN_CHANGE_STATUS_FILE = 10
-    MAX_CHANGE_STATUS_FILE = 10
+    MIN_CHANGE_STATUS_FILE = 3
+    MAX_CHANGE_STATUS_FILE = 3
 
-    READ_TRANS_STATUS = 300
-    READ_TASK_STATUS = 350
+    READ_TRANS_STATUS = 6
+    READ_TASK_STATUS = 7
 
     N_TH_STEP_1 = 3
-    N_TH_STEP_2 = 3
-    N_TH_STEP_3 = 3
-    N_TH_STEP_4 = 3
+    N_TH_STEP_2 = 4
+    N_TH_STEP_3 = 5
+    N_TH_STEP_4 = 2
     N_TH_STEP_5 = N_TH_STEP_1 
     
     print "\n"
@@ -404,6 +462,8 @@ if __name__ == "__main__":
     print "\n"
 
     time_tot=0
+
+
 
     print "\033[1;34m    STEP 1\033[1;m"
     print "\033[1;34m--------------\033[1;m"
@@ -443,7 +503,7 @@ if __name__ == "__main__":
     for i in range(1,N_TH_STEP_1+1):
         print "\n"
         print "((( THREAD ",i,")))"
-        print "TaskID: ",transID[i-1]
+        print "TransID: ",transID[i-1]
         print "Numero File creati: ",n_file[i-1]
         print "Numero Task creati: ",n_task[i-1]
         print "Numero di deadlock generati: ",dl_count_step1[i-1]
@@ -451,13 +511,14 @@ if __name__ == "__main__":
     print "\n"
     print "Time medio [insert T, add F, insert TT]: ",list_media_t," secondi"
     print "\n"
-    print "TaskID: ",taskID
     print "-------------------------------------------------------------"
     print "Impiegati", time_tot_s, "secondi per eseguire lo step 1"
     print "-------------------------------------------------------------"
     time_tot = time_tot + time_tot_s
     print "\n"
-  
+ 
+
+ 
     print "\033[1;34m    STEP 2 + STEP 3\033[1;m"
     print "\033[1;34m----------------------\033[1;m"
     th = []
@@ -472,18 +533,19 @@ if __name__ == "__main__":
         if ((min_tmp + N_TH_STEP_3) >= n_file[r]):
             # Si verifica se (e quanti) File bisogna creare per aggiungere un Task per una specifica Trasformazione
             diff_file = ((min_tmp + N_TH_STEP_3) - n_file[r]) +1 
-            # Si crea il numero giusto di File
-            for i in range(0,diff_file):
-                min_tmp += 1
-                lfns_tmp = "/aa/lfn." + str(min_tmp) + "." + str(r) + ".txt"
-                #lfns_tmp = "/aa/lfn." + str(uuid_str) + "." + str(i) + ".txt"
-                res = transClient.addFilesToTransformation( transID[r], [lfns_tmp] )
-                if res['OK']==True:
-                    print "**** Creato un File per creare un nuovo Task (per il thread ",r+1,") ****"
-                    lfns[r].append(lfns_tmp)
-                else:
-                    print "\033[1;31m#### ERROR: File per aggiungere i Task non creati ####\033[1;m"
-                    print res['Message']
+            # Si crea il numero giusto di File guardando se il numero di file da creare e' positivo
+            if (diff_file > 0):
+                for i in range(0,diff_file):
+                    min_tmp += 1
+                    #lfns_tmp = "/aa/lfn." + str(min_tmp) + "." + str(r) + ".txt"
+                    lfns_tmp = "/aa/lfn." + str(uuid_str) + "." + str(i) + ".txt"
+                    res = transClient.addFilesToTransformation( transID[r], [lfns_tmp] )
+                    if res['OK']==True:
+                        print "**** Creato un File per creare un nuovo Task (per il thread ",r+1,") ****"
+                        lfns[r].append(lfns_tmp)
+                    else:
+                        print "\033[1;31m#### ERROR: File per aggiungere i Task non creati ####\033[1;m"
+                        print res['Message']
         min_tmp = min[r]
         f_tmp = []
         for i in range(0,N_TH_STEP_3):
@@ -559,6 +621,8 @@ if __name__ == "__main__":
     time_tot = time_tot + time_tot_s
     print "\n"
 
+
+
     print "\033[1;34m    STEP 4\033[1;m"
     print "\033[1;34m--------------\033[1;m"
     th = []
@@ -566,7 +630,8 @@ if __name__ == "__main__":
     dl_count_step4 = [None]*N_TH_STEP_4
     # Instanziazione dei thread
     for i in range(1, N_TH_STEP_4 + 1):
-        t = stepFour(i, transID[i-1], n_task[i-1])
+        r = random.randint(0, N_TH_STEP_1 - 1)
+        t = stepFour(i, transID[r], n_task[r])
         th.append(t)
     # Si fanno partire i thread
     for i in range(1, N_TH_STEP_4 + 1):
@@ -600,23 +665,34 @@ if __name__ == "__main__":
     time_tot = time_tot + time_tot_s
     print "\n"
 
-    r = -1
-    r = input("SI VOGLIONO CANCELLARE LE TRANSFORMATION, FILE, TASK APPENA CREATI? [SI->1, NO->qualsiasi numero] >> ")
-    print "\n"
-    if (r == 1):
-        print "\033[1;34m    Elliminazione Transformation\033[1;m"
-        print "\033[1;34m------------------------------------\033[1;m"
-        th = []
-        # Instanziazione dei thread
-        for i in range(1, N_TH_STEP_5 + 1):
-            t = stepFive(i, transID[i-1])
-            th.append(t)
-        # Si fanno partire i thread
-        for i in range(1, N_TH_STEP_5 + 1):
-            th[i-1].start()
-        # Il main aspetta la fine di tutti i thread per proseguire la sua exec
-        for i in range(1, N_TH_STEP_5 + 1):
-            th[i-1].join()
+
+
+    ext = 0
+    while (ext != 1):
+        r = -1
+        r = input("SI VOGLIONO CANCELLARE LE TRANSFORMATION, FILE, TASK APPENA CREATI? [SI->1, NO->0] >> ")
+        print "\n"
+        if (r == 1):
+            print "\033[1;34m    Elliminazione Transformation\033[1;m"
+            print "\033[1;34m------------------------------------\033[1;m"
+            th = []
+            # Instanziazione dei thread
+            for i in range(1, N_TH_STEP_5 + 1):
+                t = stepFive(i, transID[i-1])
+                th.append(t)
+            # Si fanno partire i thread
+            for i in range(1, N_TH_STEP_5 + 1):
+                th[i-1].start()
+            # Il main aspetta la fine di tutti i thread per proseguire la sua exec
+            for i in range(1, N_TH_STEP_5 + 1):
+                th[i-1].join()
+            ext = 1
+        else:
+            if (r != 0):
+                print "\033[1;34mScelta non valida.. Prego, inserire nuovamente l'opzione\033[1;m"
+            else:
+                ext = 1
+            
     print "\n" 
     
     print "\033[1;32m-------------------------------------------------------------\033[1;m"
